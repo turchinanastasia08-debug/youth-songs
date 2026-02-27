53b2b975791d896f6306fe71482bc5b85cc9c482
Forest_youth <turchinanastasia08@gmail.com>
2026-02-27 15:01:13 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,26 @@ self.addEventListener("install", event => {
       .then(cache => cache.addAll(urlsToCache))
   );
 });
-
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
